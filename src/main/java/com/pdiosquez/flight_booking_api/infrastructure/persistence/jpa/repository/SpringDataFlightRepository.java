6a4a8a6b1c0a518079487ce550055f7ef85cd255
pdiosquez467 <pablodiosquez467@gmail.com>
2026-03-03 08:55:47 -0300

    fix(jpa): correct generic type in SpringDataFlightRepository to use FlightEntity

--- src/main/java/com/pdiosquez/flight_booking_api/infrastructure/persistence/jpa/repository/SpringDataFlightRepository.java
+++ src/main/java/com/pdiosquez/flight_booking_api/infrastructure/persistence/jpa/repository/SpringDataFlightRepository.java
@@ -1,6 +1,5 @@
 package com.pdiosquez.flight_booking_api.infrastructure.persistence.jpa.repository;
 
-import com.pdiosquez.flight_booking_api.domain.model.Flight;
 import com.pdiosquez.flight_booking_api.infrastructure.persistence.jpa.entity.FlightEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
